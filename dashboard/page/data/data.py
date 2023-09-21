9c668d5c1556fba373169bfbbd425c216e3dd2ae
Gustana <gustana.satiawan@gmail.com>
2023-09-21 11:52:36 +0700

    change dir

--- dashboard/page/data/data.py
+++ dashboard/page/data/data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('../data/order_payments_dataset.csv')
+df = pd.read_csv('data/order_payments_dataset.csv')
 
 def getDataFrame() -> pd.DataFrame:
     return df.loc[:, df.columns != 'order_id']
